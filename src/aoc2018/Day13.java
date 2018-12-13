31ce9eaced9b97d967b34d19b6e0a772873781f7
Oliver Springauf <Oliver.SA.Springauf@bmwgroup.com>
2018-12-13 11:04:26 +0100

    day 13 improved

--- src/aoc2018/Day13.java
+++ src/aoc2018/Day13.java
@@ -1,11 +1,10 @@
 package aoc2018;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-
 import static org.jooq.lambda.Seq.range;
 import static org.jooq.lambda.Seq.seq;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Day13 {
 
@@ -19,81 +18,67 @@ public class Day13 {
 	}
 	
 	static class Cart {
+		int x, y;
+		char dir;
+		int turn = 0; // 0=left, 1=straight, 2=right repeat
+
 		public Cart(Integer x2, Integer y2, char c) {
 			x = x2;
 			y = y2;
 			dir = c;
 		}
 		
-		int x, y;
-		char dir;
-		int turn = 0; // 0=left, 1=straight, 2=right repeat
-		
-		
-		char crossing() {		
+		void turnAtCrossing() {		
 			dir = 
 					(turn == 0)? map(dir, "><^v", "^v<>") : // left
 					(turn == 2)? map(dir, "><^v", "v^><") : // right
 					dir;
 			turn = (turn + 1) % 3;
-			return dir;
+		}
+		
+		boolean samePos(Cart c) {
+			return x == c.x && y == c.y;
 		}
 		
 		void move() {
 			if (dir=='>') {
 				x++;
 				switch(tracks[x][y]) {
-				case '-':
-					break;
-				case '\\':
-					dir='v'; break;
-				case '/':
-					dir='^'; break;
-				case '+':
-					crossing(); break;
-				case '|': throw new IllegalStateException();
+				case  '-': break;
+				case '\\': dir='v'; break;
+				case  '/': dir='^'; break;
+				case  '+': turnAtCrossing(); break;
+				case  '|': throw new IllegalStateException();
 				}
 			}
 			else if (dir=='<') {
 				x--;
 				switch(tracks[x][y]) {
-				case '-':
-					break;
-				case '\\':
-					dir='^'; break;
-				case '/':
-					dir='v'; break;
-				case '+':
-					crossing(); break;
-				case '|': throw new IllegalStateException();
+				case  '-': break;
+				case '\\': dir='^'; break;
+				case  '/': dir='v'; break;
+				case  '+': turnAtCrossing(); break;
+				case  '|': throw new IllegalStateException();
 				}
 			}
 			else if (dir=='^') {
 				y--;
 				switch(tracks[x][y]) {
-				case '|':
-					break;
-				case '\\':
-					dir='<'; break;
-				case '/':
-					dir='>'; break;
-				case '+':
-					crossing(); break;
-				case '-': throw new IllegalStateException();
+				case  '|': break;
+				case '\\': dir='<'; break;
+				case  '/': dir='>'; break;
+				case  '+': turnAtCrossing(); break;
+				case  '-': throw new IllegalStateException();
 				}
 			}
 			else if (dir=='v') {
 				y++;
 				switch(tracks[x][y]) {
-				case '|':
-					break;
-				case '\\':
-					dir='>'; break;
-				case '/':
-					dir='<'; break;
-				case '+':
-					crossing(); break;
-				case '-': throw new IllegalStateException();
+				case  '|': break;
+				case '\\': dir='>'; break;
+				case  '/': dir='<'; break;
+				case  '+': turnAtCrossing(); break;
+				case  '-': throw new IllegalStateException();
 				}
 			}
 		}
@@ -106,12 +91,12 @@ public class Day13 {
 		height = l.size();
 		tracks = new char[width][height];
 		
-		// extract carts
-		range(0, width).forEach(x -> range(0, height).forEach(y -> {
+		// build tracks and extract carts
+		range(0, height).forEach(y -> range(0, width).forEach(x -> {
 			char c = l.get(y).charAt(x);
-			if (seq("^v><".chars()).contains((int) c)) {
+			if ("^v><".indexOf(c) >= 0) { // cart at this position
 				carts.add(new Cart(x, y, c));
-				c = (c=='^'||c=='v') ? '|' : '-';
+				c = (c=='^'||c=='v') ? '|' : '-'; // track under cart
 			}
 			tracks[x][y] = c;
 		}));		
@@ -140,20 +125,23 @@ public class Day13 {
 	}
 	
 	static void crashCheck(Cart c) {
-		if (seq(carts).count(z -> z.x==c.x && z.y==c.y) > 1) { 
+		if (seq(carts).count(z -> z.samePos(c)) > 1) { 
 			//throw new IllegalStateException("crash at " + c.x + "," + c.y);
 			System.err.println("crash at " + c.x + "," + c.y);
-			carts.removeIf(k -> k.x==c.x && k.y==c.y);
+			carts.removeIf(k -> k.samePos(c));
 		}
 	}
 
 	private static void printTrack() {
 		range(0, height).forEach(y -> {
 			System.out.println(
-			range(0, width).map(x -> {
-				Optional<Cart> c = seq(carts).findFirst(z -> z.x==x && z.y==y);
-				return c.isPresent() ? c.get().dir : tracks[x][y];				
-			}).toString());
+				range(0, width).map(x ->
+					// cart here? --> cart.direction else track
+					seq(carts)
+						.findFirst(z -> z.x==x && z.y==y)
+						.map(z -> z.dir)
+						.orElse(tracks[x][y])				
+				).toString());
 		});
 		System.out.println();
 	}
