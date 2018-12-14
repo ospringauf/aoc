02a6a7a602610f5d25facc3417b33a72d73c4355
Oliver Springauf <ospringauf@gmx.de>
2018-12-14 08:22:59 +0100

    day 14 pretty

--- src/aoc2018/Day14.java
+++ src/aoc2018/Day14.java
@@ -5,24 +5,21 @@ import static org.jooq.lambda.Seq.seq;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.jooq.lambda.Seq;
-
 public class Day14 {
 
-	static int e1 = 0;
-	static int e2 = 1;
+	static int elf1 = 0;
+	static int elf2 = 1;
 	static List<Integer> score = new ArrayList<>();
 	
 	
-	
 	static void next() {
-		int res = score.get(e1) + score.get(e2);
+		int res = score.get(elf1) + score.get(elf2);
 		
-		List<Integer> digits = Seq.seq(Integer.toString(res).chars()).map(i -> i - '0').toList();
+		List<Integer> digits = seq(Integer.toString(res).chars()).map(i -> i - '0').toList();
 		score.addAll(digits);
 		
-		e1 = (e1 + 1 + score.get(e1)) % score.size();
-		e2 = (e2 + 1 + score.get(e2)) % score.size();
+		elf1 = (elf1 + 1 + score.get(elf1)) % score.size();
+		elf2 = (elf2 + 1 + score.get(elf2)) % score.size();
 	}
 	
 	public static void main(String[] args) throws Exception {
@@ -33,10 +30,9 @@ public class Day14 {
 		part1();
 		System.out.println("=== part 2 ===");
 		part2();
-		
 	}
 
-	protected static void part1() {
+	static void part1() {
 		int input = 293801;
 		while (score.size() < input + 10) {
 //			System.out.println(seq(score).toString(" "));
@@ -46,7 +42,7 @@ public class Day14 {
 		System.out.println(seq(score).skip(input).take(10).toString());
 	}
 
-	private static void part2() throws Exception {
+	static void part2() throws Exception {
 		String input = "293801";
 //		String input = "59414";
 		int[] sub = seq(input.chars()).mapToInt(i -> i-'0').toArray();
@@ -71,5 +67,4 @@ public class Day14 {
 		//20280190
 		System.out.println(p-sub.length);
 	}
-
 }
