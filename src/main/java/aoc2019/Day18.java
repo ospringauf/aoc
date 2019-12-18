f65902001c608ae585612ef41fccb124527bef20
Oliver Springauf <Oliver.SA.Springauf@bmwgroup.com>
2019-12-18 12:35:29 +0100

    in progress

--- src/main/java/aoc2019/Day18.java
+++ src/main/java/aoc2019/Day18.java
@@ -42,37 +42,63 @@ public class Day18 {
 //        var collectedKeys = new HashSet<Character>();
 //        var openDoors = new HashSet<Character>();
         
-        var steps = bestPath(pos, keys); 
+        var steps = bestPath(pos, keys, 0); 
         
         map.print();
         System.out.println(steps);        
     }
 
-	private int bestPath(Point pos, Set<Character> keys) {
-		var steps = 0;
-        if ( ! keys.isEmpty())  
-        {
-        	var locked = keys.stream().map(k -> doorOf(k)).collect(Collectors.toSet());
-        	var dist = map.minDistances(pos, c -> c!=WALL && !locked.contains(c));
-        	var reachKeys = keys.stream().filter(k -> dist.containsKey(map.findPoint(k))).collect(Collectors.toList());
-        	
-        	var key = reachKeys.get(0);
-        	System.out.println("select key: " + key + " from " + reachKeys);
-//        	pos = map.findPoint(key); 
-//        	steps += dist.get(pos);
-//        	keys.remove(key);
-        	var next = map.findPoint(key);
-        	var s = bestPath(next, except(keys, key));
-        	steps += dist.get(next) + s;
-
-//        	map.put(pos, '.');
-//        	var dk = map.findPoint(doorOf(key));
-//        	if (dk != null) map.put(dk, '.');
+    int bound = Integer.MAX_VALUE / 2;
+    
+	private int bestPath(Point pos, Set<Character> keys, int fre) {
+//		var steps = 0;
+        if (keys.isEmpty()) {
+            if (fre < bound) bound = fre;
+            return fre;
         }
+        
+    	var locked = keys.stream().map(Day18::doorOf).collect(Collectors.toSet());
+    	var dist = map.minDistances(pos, c -> c != WALL && !locked.contains(c));
+    	var reachKeys = keys.stream().filter(k -> dist.containsKey(map.findPoint(k))).collect(Collectors.toList());
+    	
+    	var best = 1000000; //Integer.MAX_VALUE;
+    	for (var key : reachKeys) {
+//                System.out.println("select key: " + key + " from " + reachKeys);
+            var nextPos = map.findPoint(key);
+            int dn = dist.get(nextPos);
+            if (fre + dn > bound) continue;
+            var s = bestPath(nextPos, except(keys, key), fre + dn);
+            best = Math.min(best, s);
+    	}
+        var steps = fre + best;                
+    
 		return steps;
 	}
 	
-	
+	private int bestPath0(Point pos, Set<Character> keys) {
+        var steps = 0;
+        if ( ! keys.isEmpty())  
+        {
+            var locked = keys.stream().map(k -> doorOf(k)).collect(Collectors.toSet());
+            var dist = map.minDistances(pos, c -> c!=WALL && !locked.contains(c));
+            var reachKeys = keys.stream().filter(k -> dist.containsKey(map.findPoint(k))).collect(Collectors.toList());
+            
+//          for (var key : )
+            var key = reachKeys.get(0);
+            System.out.println("select key: " + key + " from " + reachKeys);
+//          pos = map.findPoint(key); 
+//          steps += dist.get(pos);
+//          keys.remove(key);
+            var next = map.findPoint(key);
+            var s = bestPath0(next, except(keys, key));
+            steps += dist.get(next) + s;
+
+//          map.put(pos, '.');
+//          var dk = map.findPoint(doorOf(key));
+//          if (dk != null) map.put(dk, '.');
+        }
+        return steps;
+    }
 
 	Set<Character> except(Set<Character> keys, Character key) {
 		var s = new HashSet<Character>(keys);
