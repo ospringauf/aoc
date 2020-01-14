df2402d1ea0255bc210fb2c60a06e64fe3da1d3d
Oliver Springauf <ospringauf@gmx.de>
2020-01-14 07:40:54 +0100

    var rename

--- src/main/julia/day04.jl
+++ src/main/julia/day04.jl
@@ -1,12 +1,12 @@
 # part 1
 
-mono(a) = all(i -> a[i]<=a[i+1], [1:5;])
+mono(A) = all(i -> A[i]<=A[i+1], [1:5;])
 
-repeated(a) = any(i -> a[i]==a[i+1], [1:5;])
+repeated(A) = any(i -> A[i]==A[i+1], [1:5;])
 
 function passwd1(n) 
-    a = reverse(digits(n))
-    mono(a) && repeated(a)
+    A = reverse(digits(n))
+    mono(A) && repeated(A)
 end    
 
 @time length(filter(passwd1, [156218:652527;]))
@@ -14,10 +14,10 @@ end
 # part 2
 
 # pair(a) = any(i -> count(x -> x==a[i], a) == 2, [1:6;])
-pair(a) = 2 ∈ map(d -> count(x -> x==d, a), a)
+pair(A) = 2 ∈ map(d -> count(x -> x==d, A), A)
 
-passwd2(n) = ( a = reverse(digits(n)) ; mono(a) && pair(a) )
+passwd2(n) = ( A = reverse(digits(n)) ; mono(A) && pair(A) )
 
-@time [156218:652527;] |> x->passwd2.(x) |> count
+@time [156218:652527;] |> n -> passwd2.(n) |> count
 
 
