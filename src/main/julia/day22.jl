b82ec6236a273abaa03ae4b3578ba1d0c0e6d2ec
Oliver Springauf <ospringauf@gmx.de>
2020-01-22 01:24:09 +0100

    julia day22

--- src/main/julia/day22.jl
+++ src/main/julia/day22.jl
@@ -17,7 +17,6 @@ function parserule(s)
 end
 
 # A B C = C * ( B * A) 
-
 combine(A,B) = (B*A) .% L
 
 T = foldl(combine, parserule.(rules))
@@ -25,25 +24,31 @@ T = foldl(combine, parserule.(rules))
 part1 = first(T * [2019; 1]) % L
 
 
-# part 2
 
-A=[2 1;0 1]
-0*A
+
+
+# part 2
 
 L = 119315717514047
 n = 101741582076661
 
-function powMod(x, N)::Int128
+# function powMod(x::Int, N)::Int128
+#     if N==1 
+#         return x
+#     end
+#     p = powMod(x, N÷2) % L
+#     (N%2==0) ? p*p % L : x*p*p % L
+# end
+
+function matpowermod(A::Array, N)::Array
     if N==1
-        return x
+        return A
     end
-    p = powMod(x, N÷2) % L
-    (N%2==0) ? p*p % L : x*p*p % L
+    P = matpowermod(A, N÷2) .% L
+    (N%2 == 0) ? (P*P).%L : ((P*P.%L)*A).%L
 end
 
-# powMod(2,L-2)
-
-function parseruleInverse(s) 
+function parseruleInverse(s)::Array{Int128,2} 
     if startswith(s, "cut")
         # i -> i - c
         c = parse(Int, s[4:end])
@@ -54,7 +59,13 @@ function parseruleInverse(s)
     else
         # i -> (c * i) % L
         c = parse(Int, s[length("deal with increment "):end])
-        f = powMod(c, L-2)
-        [c 0; 0 1]
+        # f = powMod(c, L-2) # Fermat: Division in Primzahlkörper
+        f = powermod(c, L-2, L)
+        [f 0; 0 1]
     end
 end
+
+T = foldl(combine, reverse(parseruleInverse.(rules)))
+T = matpowermod(T, n)
+
+part2 = first(T*[2020; 1]) .%L 
