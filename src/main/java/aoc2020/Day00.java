5a42f9b852cefbb06ac94e6f6a1ff992cbc8f6ad
Oliver Springauf <ospringauf@gmx.de>
2020-11-26 07:19:15 +0100

    vavr experiment

--- src/main/java/aoc2020/Day00.java
+++ src/main/java/aoc2020/Day00.java
@@ -1,7 +1,7 @@
 package aoc2020;
 
 import java.util.Arrays;
-import java.util.stream.Stream;
+import java.util.HashSet;
 
 import io.vavr.collection.List;
 
@@ -15,26 +15,54 @@ public class Day00 {
 			e
 			""";
 
-	String instr = "L3, R1, L4, L1, L2, R4, L3, L3, R2, R3, L5, R1, R3, L4, L1, L2, R2, R1, L4, L4, R2, L5, R3, R2, R1, L1, L2, R2, R2, L1, L1, R2, R1, L3, L5, R4, L3, R3, R3, L5, L190, L4, R4, R51, L4, R5, R5, R2, L1, L3, R1, R4, L3, R1, R3, L5, L4, R2, R5, R2, L1, L5, L1, L1, R78, L3, R2, L3, R5, L2, R2, R4, L1, L4, R1, R185, R3, L4, L1, L1, L3, R4, L4, L1, R5, L5, L1, R5, L1, R2, L5, L2, R4, R3, L2, R3, R1, L3, L5, L4, R3, L2, L4, L5, L4, R1, L1, R5, L2, R4, R2, R3, L1, L1, L4, L3, R4, L3, L5, R2, L5, L1, L1, R2, R3, L5, L3, L2, L1, L4, R4, R4, L2, R3, R1, L2, R1, L2, L2, R3, R3, L1, R4, L5, L3, R4, R4, R1, L2, L5, L3, R1, R4, L2, R5, R4, R2, L5, L3, R4, R1, L1, R5, L3, R1, R5, L2, R1, L5, L2, R2, L2, L3, R3, R3, R1";
+//	String instr = "R8, R4, R4, R8";
+	 String instr = "L3, R1, L4, L1, L2, R4, L3, L3, R2, R3, L5, R1, R3, L4, L1, L2, R2, R1, L4, L4, R2, L5, R3, R2, R1, L1, L2, R2, R2, L1, L1, R2, R1, L3, L5, R4, L3, R3, R3, L5, L190, L4, R4, R51, L4, R5, R5, R2, L1, L3, R1, R4, L3, R1, R3, L5, L4, R2, R5, R2, L1, L5, L1, L1, R78, L3, R2, L3, R5, L2, R2, R4, L1, L4, R1, R185, R3, L4, L1, L1, L3, R4, L4, L1, R5, L5, L1, R5, L1, R2, L5, L2, R4, R3, L2, R3, R1, L3, L5, L4, R3, L2, L4, L5, L4, R1, L1, R5, L2, R4, R2, R3, L1, L1, L4, L3, R4, L3, L5, R2, L5, L1, L1, R2, R3, L5, L3, L2, L1, L4, R4, R4, L2, R3, R1, L2, R1, L2, L2, R3, R3, L1, R4, L5, L3, R4, R4, R1, L2, L5, L3, R1, R4, L2, R5, R4, R2, L5, L3, R4, R1, L1, R5, L3, R1, R5, L2, R1, L5, L2, R2, L2, L3, R3, R3, R1";
 
 	public static void main(String[] args) {
+		System.out.println("=== part 1");
 		new Day00().part1();
+		System.out.println("=== part 2");
+		new Day00().part2();
 	}
 
 	private void part1() {
-		List<Heading> headings = List.of(Heading.NORTH, Heading.EAST, Heading.SOUTH, Heading.WEST);
 		Heading head = Heading.NORTH;
-		Point pos = new Point(0,0);
-		for (String s :instr.split(", ")) {
+		Point pos = new Point(0, 0);
+
+		for (String s : instr.split(", ")) {
 			int dist = Integer.valueOf(s.substring(1));
 
-			head = head.turn(s.charAt(0)=='R');
-				
+			head = head.turn(s.charAt(0) == 'R');
+
 			pos = pos.next(head, dist);
 		}
-		
+
 		System.out.println(pos);
 		System.out.println(pos.manhattan());
+
+	}
+
+	private void part2() {
+		Heading head = Heading.NORTH;
+		Point pos = new Point(0, 0);
+		var visited = new HashSet<Point>();
+
+		for (String s : instr.split(", ")) {
+			head = head.turn(s.charAt(0) == 'R');
+
+			int dist = Integer.valueOf(s.substring(1));
+			
+			for (int i : List.range(0, dist)) {
+
+				pos = pos.next(head, 1);
+
+				if (visited.contains(pos))
+					System.out.println("seen: " + pos + " - d=" + pos.manhattan());
+
+				visited.add(pos);
+			}
+
+		}
 
 	}
 
