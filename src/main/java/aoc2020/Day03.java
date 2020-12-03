3ee843e4741548079adbe11410500fb5ecebcac9
Oliver Springauf <ospringauf@gmx.de>
2020-12-03 07:21:44 +0100

    day 3

--- src/main/java/aoc2020/Day03.java
+++ src/main/java/aoc2020/Day03.java
@@ -1,27 +1,69 @@
 package aoc2020;
 
-import io.vavr.collection.List;
+import java.util.function.UnaryOperator;
+
+import io.vavr.collection.Stream;
 
 // https://adventofcode.com/2020/day/3
-@SuppressWarnings( { "deprecation", "preview" })
+// --- Day 3: Toboggan Trajectory ---
+
+@SuppressWarnings({ "deprecation", "preview" })
 public class Day03 {
 
+	private static final char TREE = '#';
+
+
 	public static void main(String[] args) throws Exception {
+		new Day03().solve();
+	}
+
+	private void solve() throws Exception {
+		var data = example.split("\\n");
+//		var data = Util.lines("input03.txt");
+		
+		var m = new PointMap<Character>();
+		m.read(data, c -> c);
+
+		UnaryOperator<Point> nextPoint = p -> p.east(3).south(1);		
+		long t1 = countTreesOnSlope(m, nextPoint);
+		
 		System.out.println("=== part 1");
-		new Day03().part1();
+		System.out.println(t1);
+
 		System.out.println("=== part 2");
-		new Day03().part2();
+		long t2 = countTreesOnSlope(m, p -> p.east(1).south(1));
+		long t3 = countTreesOnSlope(m, p -> p.east(5).south(1));
+		long t4 = countTreesOnSlope(m, p -> p.east(7).south(1));
+		long t5 = countTreesOnSlope(m, p -> p.east(1).south(2));
+		System.out.println(t1 * t2 * t3 * t4 * t5);
 	}
 
-	private void part1() throws Exception {
-	}
-	
-	private void part2() throws Exception {
+	private long countTreesOnSlope(PointMap<Character> m, UnaryOperator<Point> nextPoint) {
+		BoundingBox bb = m.boundingBox();
+		
+		var path = Stream
+				.iterate(new Point(0,0), nextPoint)
+				.map(bb::wrapX)
+				.takeWhile(bb::contains)
+				;
+		
+		long trees = path.count(p -> m.get(p) == TREE);
+		
+		return trees;
 	}
 
-static String inp0 = """
-		""";	
 
-static String inp = """
-		""";
+	static String example = """
+			..##.......
+			#...#...#..
+			.#....#..#.
+			..#.#...#.#
+			.#...##..#.
+			..#.##.....
+			.#.#.#....#
+			.#........#
+			#.##...#...
+			#...##....#
+			.#..#...#.#
+					""";
 }
