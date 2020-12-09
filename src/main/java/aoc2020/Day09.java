a9506e8947c01cb908817ca61418aa98cebd54fa
Oliver Springauf <ospringauf@gmx.de>
2020-12-09 07:28:49 +0100

    day 9 nicer

--- src/main/java/aoc2020/Day09.java
+++ src/main/java/aoc2020/Day09.java
@@ -1,5 +1,6 @@
 package aoc2020;
 
+import io.vavr.collection.Array;
 import io.vavr.collection.List;
 
 // https://adventofcode.com/2020/day/9
@@ -8,71 +9,83 @@ import io.vavr.collection.List;
 class Day09 extends AocPuzzle {
 
 	public static void main(String[] args) {
-		new Day09().solve();
-	}
-	
-//		int preamble = 5;
-//		List<Long> data = List.of(example.split("\n")).map(Long::valueOf);
-	
-	int preamble = 25;
-	List<Long> data = lines("input09.txt").map(Long::valueOf);
+		var t0 = System.currentTimeMillis();
 
-
-	void solve() {
-		
 		System.out.println("=== part 1");
+		var result1 = new Day09().part1();
+		System.out.println(result1); // 29221323
+		System.out.format("=== end (%d ms)\n", System.currentTimeMillis() - t0);
 
-		var r = data
-				.sliding(preamble+1)
-				.map(w -> w.reverse())
-				.filterNot(w -> check1(w.head(), w.tail()))
-				.map(w -> w.head());
-		
-		long part1 = r.get();
-		System.out.println(part1);
-		
 		System.out.println("=== part 2");
+		var result2 = new Day09().part2(result1);
+		System.out.println(result2); // 4389369
+		System.out.format("=== end (%d ms)\n", System.currentTimeMillis() - t0);
+	}
+
+//	int preamble = 5;
+//	Array<Long> data = Array.of(example.split("\n")).map(Long::valueOf);
+
+	int preamble = 25;
+	Array<Long> data = lines("input09.txt").map(Long::valueOf).toArray();
+
+	long part1() { 
+
+		var r = data.sliding(preamble + 1)
+				.filterNot(w -> check1(w))
+				.map(w -> w.last());
 		
-		var part2 = List.range(0, data.size()).map(from -> check2(part1, from, data)).filter(l -> ! l.isEmpty());
-		System.out.println(part2);
-		var r2 = part2.get();
-		System.out.println(r2.min().get() + r2.max().get());
+		return r.get();
 	}
 	
-	List<Long> check2(long n, int from, List<Long> l) {
-		List<List<Long>> r = List.range(0, l.length()-from).map(x -> l.subSequence(from, from+x)).filter(s -> s.sum().longValue()==n);
-		return r.getOrElse(List.empty());
-	}
-	
-
-	private boolean check1(Long sum, List<Long> numbers) {
-		return numbers.exists(x -> numbers.contains(sum-x));
+	private boolean check1(Array<Long> window) {
+		Long sum = window.last();
+		return window.exists(x -> window.contains(sum - x));
 	}
 
+	long part2(long sum) {
 
-	void part2() {	
+		var r = List.range(0, data.size())
+				.map(from -> check2(sum, from, data))
+				.filterNot(Array::isEmpty)
+				.get();
+		
+		System.out.println(r);
+
+		return r.min().get() + r.max().get();
 	}
+
+	Array<Long> check2(long sum, int from, Array<Long> l) {
+		var r = List.range(from, l.size())
+				.filter(to -> l.subSequence(from, to).sum().longValue() == sum)
+				.headOption();
+		
+		Array<Long> r2 = r.map(to -> l.subSequence(from, to)).getOrElse(Array.empty());
+		return r2;
+	}
+
+
+	void part2() {}
 
 	static String example = """
-35
-20
-15
-25
-47
-40
-62
-55
-65
-95
-102
-117
-150
-182
-127
-219
-299
-277
-309
-576		
-			""";
+			35
+			20
+			15
+			25
+			47
+			40
+			62
+			55
+			65
+			95
+			102
+			117
+			150
+			182
+			127
+			219
+			299
+			277
+			309
+			576
+						""";
 }
