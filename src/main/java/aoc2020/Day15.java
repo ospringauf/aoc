53677b790af5f7a6f08c0e27f584b58b9833b229
Oliver Springauf <Oliver.SA.Springauf@bmwgroup.com>
2020-12-15 09:53:31 +0100

    day 15 cleaned

--- src/main/java/aoc2020/Day15.java
+++ src/main/java/aoc2020/Day15.java
@@ -4,7 +4,6 @@ import java.util.HashMap;
 import java.util.Map;
 
 import common.AocPuzzle;
-import io.vavr.collection.Array;
 import io.vavr.collection.List;
 
 // --- Day 15: Rambunctious Recitation ---
@@ -13,93 +12,72 @@ import io.vavr.collection.List;
 @SuppressWarnings({ "deprecation", "preview", "serial" })
 class Day15 extends AocPuzzle {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		System.out.println("=== part 1");
-		new Day15().part1();
-		
-		System.out.println("=== part 2");
-		new Day15().part2();
-	}
-	
-	
+        System.out.println("=== part 1"); // 763
+        new Day15().part1();
 
-	void part1() {
-//		var data = List.of(0,3,6);
-//		var data = List.of(3,1,2);
-		var data = List.of(0,14,1,3,7,9);
-		
-		Array<Integer> spoken = data.reverse().toArray();
-		System.out.println(spoken);
-		
-		int end = 2020;
-		int i = spoken.size();
-		int next;
-		var last = spoken.head();
-		
-		while (i< end) {
-			++i;
-			if (! spoken.tail().contains(last)) {
-//				System.out.println("not seen " + last);
-				next = 0;
-			} else {
-				var S = spoken;
-				var L = last;
-				var a = spoken.indexOf(last);
-				
-				int b = a+1;
-				while (! spoken.get(b).equals(last)) b++;
-				
-//				var b = List.range(a+1, S.size()).filter(x -> S.get(x) == L).get();
-//				System.out.println(last + " seen on " + a + " and " + b);
-				
-				next = b-a;
-			}
-			
-			spoken = spoken.prepend(next);
-			last = next;
-		}
-		System.out.println(i + " --> " + spoken);
-	}
+        System.out.println("=== part 2"); // 1876406
+        new Day15().part2();
+    }
 
-	void part2() {	
-//		var data = List.of(0,3,6);
-//		var data = List.of(3,1,2);
-		var data = List.of(0,14,1,3,7,9);
-		
-		int end = 30000000;
-//		int end = 2020;
-//		int end=10;
-		
-		Map<Integer, Integer> spokenLast = new HashMap<>();
-		Map<Integer, Integer> spokenBefore = new HashMap<>();
-		
-		int i = 0;
-		int next = 0;
-		int last = 0;
-		
-		while (i < end) {
-			int sb = spokenBefore.getOrDefault(last, -1);
-			
-			if (i < data.size())
-				next = data.get(i);
-			else {
-				if (sb == -1) {
-					next = 0;
-				} else {
-					int sl = spokenLast.get(last);
-					next = sl-sb;
-				}
-			}
-			
-			
-			spokenBefore.put(next, spokenLast.getOrDefault(next, -1));
-			spokenLast.put(next, i);
-//			System.out.println(last + " -> " + next + " / " + spokenLast + " / " + spokenBefore);
-			last = next;
-			i++;
-		}
-		System.out.println(last);
-	
-	}
+    // List<Integer> data = List.of(0, 3, 6);
+    List<Integer> data = List.of(0, 14, 1, 3, 7, 9);
+
+    void part1() {
+        // use (linked) list, last spoken number is first in list
+        var spoken = data.reverse();
+
+        int end = 2020;
+        var last = spoken.head();
+        int i = spoken.size();
+
+        while (i < end) {
+            int next = 0;
+            if (spoken.tail().contains(last)) {
+                var a = spoken.indexOf(last);
+                int age = spoken.drop(a + 1).indexOf(last) +  1;
+                next = age;
+            }
+
+            spoken = spoken.prepend(next);
+            last = next;
+            i++;
+        }
+        System.out.println(i + " --> " + spoken.head());
+    }
+
+    void part2() {
+
+        int end = 30000000;
+        // int end = 2020; // -> 763
+
+        Map<Integer, Integer> spokenLast = new HashMap<>();
+        Map<Integer, Integer> spokenBefore = new HashMap<>();
+
+        int i = 0;
+        int last = 0;
+
+        while (i < end) {
+            int next = 0;
+
+            if (i < data.size())
+                next = data.get(i);
+            else {
+                // last was spoken before?
+                int sb = spokenBefore.getOrDefault(last, -1);
+                if (sb >= 0) {
+                    int sl = spokenLast.get(last);
+                    next = sl - sb;
+                }
+            }
+
+            // shift "last" -> "before last"
+            spokenBefore.put(next, spokenLast.getOrDefault(next, -1));
+            spokenLast.put(next, i);
+            last = next;
+            i++;
+        }
+        System.out.println(end + " --> " + last);
+    }
 }
