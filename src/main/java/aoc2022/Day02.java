919d331163c8d8b8138e411c8a62d6d4310bd8fb
Oliver Springauf <ospringauf@gmx.de>
2022-12-03 09:26:59 +0100

    days 2+3

--- src/main/java/aoc2022/Day02.java
+++ src/main/java/aoc2022/Day02.java
@@ -1,28 +1,104 @@
 package aoc2022;
 
 import common.AocPuzzle;
-import common.Util;
 import io.vavr.collection.List;
 
-//--- Day x:  ---
-// https://adventofcode.com/2022/day/x
+//--- Day 2: Rock Paper Scissors ---
+// https://adventofcode.com/2022/day/2
 
 class Day02 extends AocPuzzle {
 
 	public static void main(String[] args) {
-		System.out.println("=== part 1"); 
+		System.out.println("=== part 1"); // 10941
 		new Day02().part1();
-		System.out.println("=== part 2"); 
+		System.out.println("=== part 2"); // 13071
 		new Day02().part2();
 	}
-
-	List<Integer> data = file2ints("input02.txt");
 	
+	List<String> data = file2lines("input02.txt");
+//	List<String> data = Util.splitLines(example);
+
+
+	enum Shape {
+		ROCK, PAPER, SCISSORS;
+
+		Shape beats() {
+			return switch (this) {
+			case ROCK -> SCISSORS;
+			case PAPER -> ROCK;
+			case SCISSORS -> PAPER;
+			};
+		}
+
+		Shape loses() {
+			return switch (this) {
+			case ROCK -> PAPER;
+			case PAPER -> SCISSORS;
+			case SCISSORS -> ROCK;
+			};
+		}
+
+		int score() {
+			return switch (this) {
+			case ROCK -> 1;
+			case PAPER -> 2;
+			case SCISSORS -> 3;
+			};
+		}
+
+		static Shape parse(String s) {
+			return switch (s) {
+			case "A", "X" -> ROCK;
+			case "B", "Y" -> PAPER;
+			case "C", "Z" -> SCISSORS;
+			default -> null;
+			};
+		}
+	}
+
+	int score(Shape elf, Shape mine) {
+		int score = mine.score();
+		if (mine.beats() == elf)
+			return 6 + score;
+		if (mine == elf)
+			return 3 + score;
+		return score;
+	}
+
 	void part1() {
-	    System.out.println(data.size());
-	}
-	
-	void part2() {
+		int totalScore = 0;
+
+		for (var line : data.map(split(" "))) {
+			var elf = Shape.parse(line.s(0));
+			var mine = Shape.parse(line.s(1));
+
+			totalScore += score(elf, mine);
+		}
+		System.out.println(totalScore);
 	}
 
+	void part2() {
+		int totalScore = 0;
+		
+		for (var line : data.map(split(" "))) {
+			var elf = Shape.parse(line.s(0));
+			var strategy = line.s(1);
+
+			var mine = switch (strategy) {
+			case "X" -> elf.beats(); // I lose
+			case "Y" -> elf; // draw
+			case "Z" -> elf.loses(); // I win
+			default -> null;
+			};
+
+			totalScore += score(elf, mine);
+		}
+		System.out.println(totalScore);
+	}
+
+	static String example = """
+			A Y
+			B X
+			C Z
+						""";
 }
--- src/main/java/aoc2022/Day04.java
+++ src/main/java/aoc2022/Day04.java
@@ -4,19 +4,19 @@ import common.AocPuzzle;
 import common.Util;
 import io.vavr.collection.List;
 
-//--- Day x:  ---
-// https://adventofcode.com/2022/day/x
+//--- Day 4:  ---
+// https://adventofcode.com/2022/day/4
 
-class DayXx extends AocPuzzle {
+class Day04 extends AocPuzzle {
 
 	public static void main(String[] args) {
 		System.out.println("=== part 1");
-		new DayXx().part1();
+		new Day04().part1();
 		System.out.println("=== part 2");
-		new DayXx().part2();
+		new Day04().part2();
 	}
 
-	List<Integer> data = file2ints("input0x.txt");
+	List<Integer> data = file2ints("input04.txt");
 
 	void part1() {
 		System.out.println(data.size());
