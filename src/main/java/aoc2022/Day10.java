9df86c8bc7d39b25fab36e452110d43620e41d18
oliver <oliver@picchio>
2022-12-09 07:49:50 +0100

    prep day 10

---
+++ src/main/java/aoc2022/Day10.java
@@ -0,0 +1,33 @@
+package aoc2022;
+
+import common.AocPuzzle;
+import common.Util;
+import io.vavr.collection.List;
+
+// --- Day 10:  ---
+// https://adventofcode.com/2022/day/10
+
+class Day10 extends AocPuzzle {
+
+    public static void main(String[] args) {
+        System.out.println("=== part 1");
+        new Day10().part1();
+        System.out.println("=== part 2");
+        new Day10().part2();
+    }
+
+    List<String> data = Util.splitLines(example);
+//    List<String> data = file2lines("input10.txt");
+
+    void part1() {
+        System.out.println(data.size());
+    }
+
+    void part2() {
+    }
+
+    static String example = """
+
+            """;
+
+}
