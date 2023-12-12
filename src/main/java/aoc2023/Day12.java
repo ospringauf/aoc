d5a393931090ba42a2566767951b26bc185b9de3
Oliver Springauf <Oliver.SA.Springauf@bmwgroup.com>
2023-12-12 16:48:22 +0100

    still struggling

--- src/main/java/aoc2023/Day12.java
+++ src/main/java/aoc2023/Day12.java
@@ -13,87 +13,130 @@ class Day12 extends AocPuzzle {
         System.out.println("=== part 1");
         timed(() -> new Day12().part1());
         System.out.println("=== part 2");
-        timed(() -> new Day12().part2());
+//        timed(() -> new Day12().part2());
     }
 
-    List<String> data = file2lines("input12.txt");
-//    List<String> data = Util.splitLines(example);
+//    List<String> data = file2lines("input12.txt");
+    List<String> data = Util.splitLines(example);
 
-    List<Integer> counts(String s) {
-        List<Integer> r = List.empty();
-        int i = 0;
-        int l = s.length();
-        while (i < l) {
-            while (i < l && s.charAt(i) == '.')
-                i++;
-            if (i >= l)
-                break;
-            int j = i;
-            while (i < l && s.charAt(i) == '#')
-                i++;
-            r = r.append(i - j);
-            if (i >= l)
-                break;
-        }
-        return r;
+	void part1() {
+		//var x = solve2("?###????????", List.of(3,2,1));
+		//var x = solve2("????????", List.of(2,1));
+//		var x = solve2(".", List.of());
+		var x = solve2("?????", List.of(2,1));
+		System.out.println(x);
+	}  
+	
+	int mind(String s) {
+		int r = 0;
+		for (int i=0; i<s.length(); ++i) {
+			if (s.charAt(i)=='#') r++;
+		}
+		return r;
+	}
+
+    int solve2(String s, List<Integer> l) {
+    	System.out.println("s=" + s + "  l=" + l);
+    	if (s.length() == 0) {
+    		if (l.size() == 0) return 1;
+    		if (l.size() ==1 && l.head().intValue() == 0) return 1;
+    		return 0;
+    	}
+    	if (l.isEmpty()) {
+    		return mind(s) == 0? 1: 0;
+    	}
+    	var c = s.charAt(0);
+    	var s2 = s.substring(1);
+    	
+    	var h = l.head().intValue();
+    	if (h == 0) {
+    		if (c == '#') return 0;
+    		return solve2(s2, l.tail());
+    	} else {
+    		// #
+    		if (c == '#') 
+    			return solve2(s2, List.of(h-1).appendAll(l.tail()));
+    		// .
+    		if (c == '.')
+    			return 0;
+    		// ?
+    		var r1 = solve2(s2, l); //.
+    		var r2 = solve2(s2, List.of(h-1).appendAll(l.tail())); //#
+    		return r1+r2;
+    	}
+	}
+
+	void part2() {
+   	
+//    	var l = data.last();
+//    	var springs = l.split(" ")[0] + ".";
+//    	var dmg = Util.string2ints(l.split(" ")[1].replace(',', ' '));
+//    	
+//    	var r = solve(springs, dmg, "");
+
+//    	List<Integer> dmg = List.of(1); String springs = "????.";
+//    	List<Integer> dmg = List.of(2,1); String springs = "???????.";
+    	List<Integer> dmg = List.of(3,2,1); String springs = "?###????????.";
+		var r = solve(springs, dmg, "");
+    	
+    	System.out.println("=== " + r);
+    	System.out.println(springs + " (" + dmg.mkString(",") + ") -> " + r);
+    	
     }
 
-    void part1() {
-        System.out.println();
-        
-        System.out.println("---");
-        
-        int sum =0;
-        for (var l : data) {
-            var f = l.split(" ");
-            var springs = f[0];
-            var dmg = Util.string2ints(f[1].replace(',', ' '));
+    long solve(String s, List<Integer> dmg, String pref) {
+    	if (dmg.isEmpty())
+    		return 1;
+		var dmg1 = dmg.head();
+		var rem = dmg.tail().map(x -> x+1).sum().intValue();
+		
+		if (dmg.tail().isEmpty())
+			return ways(s, dmg1);
+		
+		var min = dmg1+1;
+		var max = s.length()-rem;
+		
+		
+		var r = 0L;
+		for (int i=min; i<=max; ++i) {
+			if (s.charAt(i-1) == '#') continue;
+			var s1 = s.substring(0, i-1) + ".";
+			var s2 = s.substring(i);
+			var p1 = ways(s1, dmg1);
+			var p2 = (p1 > 0) ? solve(s.substring(i), dmg.tail(), pref + "   ") : 0;
+			System.out.println(pref+"s1="+s1+ "  s2="+s2 + "  p1="+p1 + "  dmg1="+dmg1 + "  p2="+p2);
+			r += p2;
+		}
+		System.out.println(pref + r);
+		return r;
+	}
 
-            // System.out.println(springs + " -> " + dmg);
-//            System.out.println(springs + " -> " + counts(springs) + " / " + dmg.eq(counts(springs)));
+    // each block ends with .
+    
+    
+	long ways(String s, Integer dmg) {
+		int r = 0;
+		for (int i=0; i<=s.length()-dmg; ++i) {
+			// put ####. into s at position i
+			var d = 0;
+			for (int j=0; j<s.length(); ++j) {
+				var c = s.charAt(j);
+				if (j >= i && j < i+dmg) {
+					if (c == '.') {
+						d = -1;
+						break;
+					}
+						d++;
+				} else {
+					if (c == '#') d++;
+				}
+			}
+			if (d == dmg) r++;
+		}
+		return r;
+	}
 
-            var unk = unknown(springs)*5;
-            springs=springs+springs+springs+springs+springs;
-            
-            int match = 0;
-            for (int x = 0; x < (1 << unk); ++x) {
-                var sx = fill(springs, x);
-                if (dmg.eq(counts(sx))) match++;
-            }
-            
-            System.out.println(springs + " -> " + match);
-            sum+=match;
-        }
-        
-        System.out.println(sum);
-    }
-
-    private String fill(String s, int x) {
-        var r = "";
-        for (int i = 0; i < s.length(); ++i) {
-            if (s.charAt(i) == '?') {
-                r = r + ((x % 2 == 1) ? "#" : ".");
-                x = x / 2;
-            } else {
-                r = r + s.charAt(i);
-            }
-        }
-
-        return r;
-    }
-
-    private int unknown(String s) {
-        int r = 0;
-        for (int i = 0; i < s.length(); ++i)
-            if (s.charAt(i) == '?')
-                r++;
-        return r;
-    }
-
-    void part2() {
-    }
-
-    static String example0 = """
+	static String example0 = """
             #.#.### 1,1,3
             .#...#....###. 1,1,3
             .#.###.#.###### 1,3,1,6
