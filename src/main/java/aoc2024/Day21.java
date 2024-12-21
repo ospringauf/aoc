914b7d699618996b76f23837a843583f6a749fa4
Oliver Springauf <ospringauf@gmx.de>
2024-12-21 20:49:22 +0100

    day 21 refactored

--- src/main/java/aoc2024/Day21.java
+++ src/main/java/aoc2024/Day21.java
@@ -15,8 +15,6 @@ import io.vavr.collection.List;
 
 //--- Day 21: Keypad Conundrum ---
 // https://adventofcode.com/2024/day/21
-// 
-// It works, don't ask my why
 
 class Day21 extends AocPuzzle {
 
@@ -27,76 +25,48 @@ class Day21 extends AocPuzzle {
         timed(() -> new Day21().solve(25));
     }
 
-//    List<String> data = file2lines("input21.txt");
     List<String> data = Util.splitLines(real);
+//    List<String> data = Util.splitLines(example);
+    
+    Robot robot0; // numerical keypad
+    Robot robot1; // directional keypad
+
 
     static class Robot {
-        PointMap<Character> kb;
-
+        PointMap<Character> keypad = new PointMap<>();
         Map<Tuple2<Character, Character>, List<String>> maneuvers = new HashMap<>();
 
-        Robot(PointMap<Character> kb) {
-            this.kb = kb;
+        Robot(String layout) {
+            keypad.read(Util.splitLines(layout));
+            keypad.findPoints('g').forEach(keypad::remove); // delete "gaps"
 
-            var keys = List.ofAll(kb.values());
+            var keys = List.ofAll(keypad.values());
             for (var t : keys.crossProduct()) {
                 maneuvers.put(t, allManeuvers(t));
             }
         }
 
         List<String> allManeuvers(Tuple2<Character, Character> t) {
-            Point k1 = kb.findPoint(t._1);
-            Point k2 = kb.findPoint(t._2);
-            var dist = k1.manhattan(k2);
-            var options = List.of(Direction.UP, Direction.DOWN, Direction.LEFT, Direction.RIGHT).crossProduct(dist)
-                    .toList();
+            Point p1 = keypad.findPoint(t._1);
+            Point p2 = keypad.findPoint(t._2);
+            var dist = p1.manhattan(p2);
+            var options = List.of(Direction.UP, Direction.DOWN, Direction.LEFT, Direction.RIGHT)
+                    .crossProduct(dist);
 
-            // find the [^<>v]* sequences that start at k1, stay on allowed keys and end at
-            // k2
-            List<String> r = List.empty();
+            // find the [^<>v]* input sequences that start at k1, stay on allowed keys and end at
+            // k2 (with Activate), e.g. 
+            // (2, 7) -> List(^^<A, ^<^A, <^^A)
+            // (9, A) -> List(vvvA)
+            List<String> m = List.empty();
             for (var seq : options) {
-                var points = seq.scanLeft(k1, (k, d) -> k.translate(d));
-                if (points.forAll(kb::containsKey) && points.last().equals(k2))
-                    r = r.append(seq.map(Direction::symbol).mkString() + "A");
+                var path = seq.scanLeft(p1, (k, d) -> k.translate(d));
+                if (path.forAll(keypad::containsKey) && path.last().equals(p2)) {                    
+                    String input = seq.map(Direction::symbol).mkString() + "A";
+                    m = m.append(input);
+                }
             }
-            return r;
+            return m;
         }
-
-        // all possible combinations of pairwise key maneuvers for the given key
-        // sequence (s)
-        List<String> sequences(String s) {
-            var pwm = pairs(s).map(maneuvers::get);
-            var r = pwm.reduceLeft((l1, l2) -> l1.flatMap(s1 -> l2.map(s2 -> s1 + s2)));
-
-            return r;
-        }
-
-        long cost(Tuple2<Character, Character> keys, int nrobots) {
-            if (nrobots == 1) {
-                var k1 = kb.findPoint(keys._1);
-                var k2 = kb.findPoint(keys._2);
-                return k1.manhattan(k2) + 1;
-            } else {
-                if (keys._1 == keys._2)
-                    return 1; // just press A
-
-                Function1<Tuple2<Character, Character>, Long> cost = t -> memCost.apply(t, nrobots-1);
-                
-                var options = maneuvers.get(keys);
-                var c = options.map(seq -> pairs("A" + seq).map(cost));
-                var min = c.map(x -> x.sum().longValue()).min().get();
-                
-//                var min = Long.MAX_VALUE;
-//                for (var m : maneuvers.get(keys)) {
-//                    var pairs = pairs("A" + m);
-//                    var c = pairs.map(t -> memCost.apply(t, nrobots - 1)).sum().longValue();
-//                    min = (c < min) ? c : min;
-//                }
-                return min;
-            }
-        }
-
-        Function2<Tuple2<Character, Character>, Integer, Long> memCost = Function2.of(this::cost).memoized();
     }
 
     static int numeric(String input) {
@@ -108,54 +78,59 @@ class Day21 extends AocPuzzle {
                 .toList();
     }
 
+    long cost(Tuple2<Character, Character> keys, int nrobots) {
+        var robot = robot0.maneuvers.containsKey(keys) ? robot0 : robot1;
+
+        if (nrobots == 0) {
+            var p1 = robot.keypad.findPoint(keys._1);
+            var p2 = robot.keypad.findPoint(keys._2);
+            return p1.manhattan(p2) + 1;
+            
+        } else {
+            if (keys._1 == keys._2)
+                return 1; // just press A
+
+            Function1<Tuple2<Character, Character>, Long> nextCost = t -> memCost.apply(t, nrobots - 1);
+
+            // for all possible maneuvers from k1 to k2, find the minmal total cost at the "last" robot in the chain
+            var options = robot.maneuvers.get(keys);
+            var c = options.map(seq -> pairs("A" + seq).map(nextCost));
+            var min = c.map(x -> x.sum().longValue()).min().get();
+
+            return min;
+        }
+    }
+    
+    Function2<Tuple2<Character, Character>, Integer, Long> memCost = Function2.of(this::cost).memoized();
+
+    
     void solve(int nrobots) {
-        PointMap<Character> kb0 = new PointMap<>();
-        kb0.read(Util.splitLines(KEY0));
-        kb0.findPoints('g').forEach(kb0::remove);
-
-        PointMap<Character> kb1 = new PointMap<>();
-        kb1.read(Util.splitLines(KEY1));
-        kb1.findPoints('g').forEach(kb1::remove);
-
-        var r0 = new Robot(kb0);
-        var r1 = new Robot(kb1);
-        Function1<Tuple2<Character, Character>, Long> cost = t -> r1.memCost.apply(t, nrobots);
+        robot0 = new Robot(KEYPAD0);
+        robot1 = new Robot(KEYPAD1);
 
         long result = 0;
 
-        for (String input : Util.splitLines(real)) {
-//            System.out.println(pairs(input));
+        for (String input : data) {
+            var seq = "A" + input;
+//            System.out.println(pairs(seq));
 
-            var options = r0.sequences("A" + input);
-            System.out.println(input + " sequences: " + options);
-
-
-//            var c = options.map(seq -> pairs("A" + seq).map(cost));
-//            System.out.println(c);
-//            var min = c.map(x -> x.sum().longValue()).min().get();
-
-            long min = Long.MAX_VALUE;
-            for (var seq : options) {
-                seq = "A" + seq;
-                var c = pairs(seq).map(t -> r1.memCost.apply(t, nrobots));
-                System.out.println("   " + seq + " -> " + c + " -> " + c.sum());
-                var sum = c.sum().longValue();
-                min = (sum < min) ? sum : min;
-            }
-            result += numeric(input) * min;
+            var c = pairs(seq).map(t -> cost(t, nrobots));
+//            System.out.println("   " + seq + " -> " + c + " -> " + c.sum());
+            
+            result += numeric(input) * c.sum().longValue();
         }
-
         System.out.println(result);
     }
 
-    static String KEY0 = """
+
+    static String KEYPAD0 = """
             789
             456
             123
             g0A
             """;
 
-    static String KEY1 = """
+    static String KEYPAD1 = """
             g^A
             <v>
             """;
@@ -173,6 +148,8 @@ class Day21 extends AocPuzzle {
             143A
             349A
             582A
-            964A            """;
+            964A            
+            """;
+
 
 }
